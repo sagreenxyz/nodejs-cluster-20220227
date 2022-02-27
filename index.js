209a53bbb201c3e85e2c4bd449ce851203c9c2ad
Scott Green <sagreenxyz@gmail.com>
2022-02-27 13:27:37 -0600

    waypoint

--- index.js
+++ index.js
@@ -3,4 +3,25 @@ const http = require('http');
 const numCPUs = require('os').cpus().length;
 const process = require('process');
 
-console.log(numCPUs);
+if(cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+
+    // Fork workers.
+    for(let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+    });
+} else {
+    // Workers can share any TCP connection
+    // In this case it is an HTTP server
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end('Hello World\n');
+    }).listen(8000);
+
+    console.log(`Worker ${process.pid} started`);
+}
+
